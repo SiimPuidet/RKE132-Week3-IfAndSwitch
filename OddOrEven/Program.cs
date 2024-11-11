c8187c064fbcd5b82543364b237be0f8e834801a
Siim Puidet <siim.puidet@tktk.ee>
2024-11-11 19:23:49 +0200

    add week3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,10 @@
+﻿// Palume kasutajal sisestada numbri.
+Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+// Kontrollime kas kasutaja sisestatud arv on paaris või paaritu ja anname vastava tagasiside.
+int result = userNum % 2;
+Console.WriteLine(result);
+if (result != 0) // != on eitus
+{ Console.WriteLine("User number is odd."); }
+else
+{ Console.WriteLine("User number is even."); }
---
+++ Week3IfAndSwitch/Program.cs
@@ -0,0 +1,8 @@
+﻿//modulus - % (Näitab jääki peale matemaatilist tehet. Näiteks 25/8=3 ning jääk 1)
+
+int a = 25;
+int b = 8;
+
+int c = a % b;
+
+Console.WriteLine(c);
